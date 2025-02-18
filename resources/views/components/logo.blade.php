7bd414f1de60fc5800e5b8f95875e0f594e7ee56
Kyaw <kyawzayartun0527@gmail.com>
2025-02-18 13:15:49 +0630

    feat: add guest login form

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="w-16 select-none">
-    <img src="{{ asset('storage/images/logo.png') }}" alt="Logo" class="w-full h-full object-cover">
+    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-full h-full object-cover">
 </div>
